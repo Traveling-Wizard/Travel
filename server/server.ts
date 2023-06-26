99fd412ff68e6bede6a0b476d4e572f509d59706
Paul <plee50@binghamton.edu>
2023-06-26 14:33:29 -0400

    fixed initial

---
+++ server/server.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response, NextFunction } from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const port: number = 5050;
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.use(
+  (
+    err: { log?: string; status?: number; message?: { err: string } },
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const defaultErr: {
+      log: string;
+      status: number;
+      message: { err: string };
+    } = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 400,
+      message: { err: 'An error occurred' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  }
+);
