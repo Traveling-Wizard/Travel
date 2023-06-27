16067643b023f6b0a3ee6469ad9495f6a1a2146b
Jeffrey Na <jeffreyna@jeffreys-air.myfiosgateway.com>
2023-06-27 15:43:34 -0400

    Parameterized correctly in cardController.ts

--- server/controllers/cardController.ts
+++ server/controllers/cardController.ts
@@ -14,10 +14,10 @@ interface CardController {
 const cardController: CardController = {
   getCards: async (req: Request, res: Response, next: NextFunction) => {
     const getCardQuery =
-      'SELECT u.user_id, u.username, u.name, uc.points, cc.card_name FROM users AS u INNER JOIN user_cards AS uc ON u.user_id = uc.user_id INNER JOIN card_company AS cc ON uc.card_id = cc.card_id WHERE u.user_id = target_id;';
+      'SELECT uc.points, cc.card_name FROM users AS u INNER JOIN user_cards AS uc ON u.user_id = uc.user_id INNER JOIN card_company AS cc ON uc.card_id = cc.card_id WHERE u.user_id = 1;';
     try {
       const cards = await db.query(getCardQuery);
-      res.locals.cards = cards;
+      res.locals.cards = cards.rows;
       return next();
     } catch (err) {
       return next({
@@ -30,10 +30,11 @@ const cardController: CardController = {
 
   createCard: async (req: Request, res: Response, next: NextFunction) => {
     const { user_id, card_name, points } = req.body;
+    const createArr = [ user_id, card_name, points ];
     const createCardQuery =
       'INSERT INTO user_cards (user_id, card_id, points) SELECT $1, card_id, $3 FROM card_company WHERE card_name = $2;';
     try {
-      await db.query(createCardQuery);
+      await db.query(createCardQuery, createArr);
       return next();
     } catch (err) {
       return next({
@@ -46,10 +47,11 @@ const cardController: CardController = {
 
   updateCard: async (req: Request, res: Response, next: NextFunction) => {
     const { user_id, card_id, points } = req.body;
+    const updateArr = [ user_id, card_id, points ];
     const updateCardQuery =
       'UPDATE user_cards SET points = $3 WHERE user_id = $1 AND card_id = $2;';
     try {
-      await db.query(updateCardQuery);
+      await db.query(updateCardQuery, updateArr);
       return next();
     } catch (err) {
       return next({
@@ -62,10 +64,11 @@ const cardController: CardController = {
 
   deleteCard: async (req: Request, res: Response, next: NextFunction) => {
     const { user_id, card_id } = req.body;
+    const deleteArr = [ user_id, card_id ];
     const deleteCardQuery =
       'DELETE FROM user_cards WHERE user_id = $1 AND card_id = $2;';
     try {
-      await db.query(deleteCardQuery);
+      await db.query(deleteCardQuery, deleteArr);
       return next();
     } catch (err) {
       return next({
