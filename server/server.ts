f0001ebb82f5bdbdf16e3c6d35d0886e9f067d97
Jeffrey Na <jeffreyna@jeffreys-air.myfiosgateway.com>
2023-06-27 15:44:08 -0400

    Listening on port and imported card route

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+const cardRouter = require('./routes/cards');
 
 const port: number = 5050;
 const app = express();
@@ -8,6 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+
+app.use('/cards', cardRouter);
 
 app.use(
   (
@@ -30,3 +33,5 @@ app.use(
     return res.status(errorObj.status).json(errorObj.message);
   }
 );
+
+app.listen(port);
