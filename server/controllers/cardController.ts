316ac2d7b56411e7dab155c799b6d03c09962bc7
Jeffrey Na <jeffreyna@jeffreys-air.myfiosgateway.com>
2023-06-27 14:51:30 -0400

    Create and update functionality implemented in cardController.ts

--- server/controllers/cardController.ts
+++ server/controllers/cardController.ts
@@ -3,10 +3,9 @@ import db from '../models/travelModel';
 
 interface CardController {
   getCards(req: Request, res: Response, next: NextFunction): Promise<void>;
-
-  // createCard(req: Request, res: Response, next: NextFunction): Promise<void>;
-  // updateCard(req: Request, res: Response, next: NextFunction): Promise<void>;
-  // deleteCard(req: Request, res: Response, next: NextFunction): Promise<void>;
+  createCard(req: Request, res: Response, next: NextFunction): Promise<void>;
+  updateCard(req: Request, res: Response, next: NextFunction): Promise<void>;
+  deleteCard(req: Request, res: Response, next: NextFunction): Promise<void>;
 }
 
 /**
@@ -14,24 +13,54 @@ interface CardController {
  */
 const cardController: CardController = {
   getCards: async (req: Request, res: Response, next: NextFunction) => {
-    const cardQuery =
+    const getCardQuery =
       'SELECT u.user_id, u.username, u.name, uc.points, cc.card_name FROM users AS u INNER JOIN user_cards AS uc ON u.user_id = uc.user_id INNER JOIN card_company AS cc ON uc.card_id = cc.card_id WHERE u.user_id = target_id;';
     try {
-      const card = db.query(cardQuery);
-      res.locals.cards = await card;
+      const cards = await db.query(getCardQuery);
+      res.locals.cards = cards;
       return next();
     } catch (err) {
       return next({
-        log: 'Error in cardController method',
+        log: 'Error in cardController.getCards method',
         status: 400,
         message: 'Error while loading items',
       });
     }
   },
 
-  // createCard: async (req: Request, res: Response, next: NextFunction) => {}
-  // updateCard: async (req: Request, res: Response, next: NextFunction) => {}
-  // deleteCard: async (req: Request, res: Response, next: NextFunction) => {}
+  createCard: async (req: Request, res: Response, next: NextFunction) => {
+    const { user_id, card_name, points } = req.body;
+    const createCardQuery =
+      'INSERT INTO user_cards (user_id, card_id, points) SELECT $1, card_id, $3 FROM card_company WHERE card_name = $2;';
+    try {
+      await db.query(createCardQuery);
+      return next();
+    } catch (err) {
+      return next({
+        log: 'Error in cardController.createCard method',
+        status: 400,
+        message: 'Error while loading items',
+      });
+    }
+  },
+
+  updateCard: async (req: Request, res: Response, next: NextFunction) => {
+    const { user_id, card_id, points } = req.body;
+    const updateCardQuery =
+      'UPDATE user_cards SET points = $3 WHERE user_id = $1 AND card_id = $2;';
+    try {
+      await db.query(updateCardQuery);
+      return next();
+    } catch (err) {
+      return next({
+        log: 'Error in cardController.updateCard method',
+        status: 400,
+        message: 'Error while loading items',
+      });
+    }
+  },
+
+  deleteCard: async (req: Request, res: Response, next: NextFunction) => {},
 };
 
 export default cardController;
