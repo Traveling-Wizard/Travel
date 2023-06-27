ab1e67bd8bfeb55c1d1ca38783f174487ca48a48
Jeffrey Na <jeffreyna@jeffreys-air.myfiosgateway.com>
2023-06-27 12:08:31 -0400

    GET functionality working

--- server/controllers/cardController.ts
+++ server/controllers/cardController.ts
@@ -1,4 +1,4 @@
-import {Request, Response, NextFunction} from 'express';
+import { Request, Response, NextFunction } from 'express';
 import db from '../models/travelModel';
 
 interface CardController {
@@ -9,8 +9,25 @@ interface CardController {
   // deleteCard(req: Request, res: Response, next: NextFunction): Promise<void>;
 }
 
+/**
+ * @desc Responsible for CRUD functionality of user cards.
+ */
 const cardController: CardController = {
-  getCards: async (req: Request, res: Response, next: NextFunction) => {},
+  getCards: async (req: Request, res: Response, next: NextFunction) => {
+    const cardQuery =
+      'SELECT u.user_id, u.username, u.name, uc.points, cc.card_name FROM users AS u INNER JOIN user_cards AS uc ON u.user_id = uc.user_id INNER JOIN card_company AS cc ON uc.card_id = cc.card_id WHERE u.user_id = target_id;';
+    try {
+      const card = db.query(cardQuery);
+      res.locals.cards = await card;
+      return next();
+    } catch (err) {
+      return next({
+        log: 'Error in cardController method',
+        status: 400,
+        message: 'Error while loading items',
+      });
+    }
+  },
 
   // createCard: async (req: Request, res: Response, next: NextFunction) => {}
   // updateCard: async (req: Request, res: Response, next: NextFunction) => {}
