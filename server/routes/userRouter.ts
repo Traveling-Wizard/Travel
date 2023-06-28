d245107065edd32b26f4dadcfc3a0cda45f8c49e
Paul <plee50@binghamton.edu>
2023-06-28 15:32:00 -0400

    login and signup jwt added

--- server/routes/userRouter.ts
+++ server/routes/userRouter.ts
@@ -4,12 +4,9 @@ import authenticateController from '../controllers/authenticateController';
 
 const userRouter = express.Router();
 
-userRouter.post('/', userController.getUser, (req: Request, res: Response) => {
-  return res.status(200).json(res.locals.user);
-});
-
 userRouter.post(
-  '/generate-token',
+  '/',
+  userController.getUser,
   authenticateController.generateToken,
   (req: Request, res: Response) => {
     return res.status(200).json(req.cookies.jwtToken);
@@ -17,26 +14,35 @@ userRouter.post(
 );
 
 userRouter.post(
-  '/protected',
-  authenticateController.authenticateToken,
-  (req: Request, res: Response) => {
-    const user_id = res.locals.userToken.user_id;
-
-    // Return the user_id in the response
-    return res.json({ user_id });
-  }
-);
-
-userRouter.post('/logout', (req: Request, res: Response) => {
-  res.clearCookie('jwtToken');
-  return res.status(200).json({ message: 'Logged out successfully' });
-});
-
-userRouter.post(
   '/signup',
   userController.createUser,
+  authenticateController.generateToken,
   (req: Request, res: Response) => {
-    return res.status(200).json(res.locals.user);
+    return res.status(200).json(req.cookies.jwtToken);
   }
 );
+// userRouter.post(
+//   '/generate-token',
+//   authenticateController.generateToken,
+//   (req: Request, res: Response) => {
+//     return res.status(200).json(req.cookies.jwtToken);
+//   }
+// );
+
+// userRouter.post(
+//   '/protected',
+//   authenticateController.authenticateToken,
+//   (req: Request, res: Response) => {
+//     const user_id = res.locals.userToken.user_id;
+
+//     // Return the user_id in the response
+//     return res.json({ user_id });
+//   }
+// );
+
+// userRouter.post('/logout', (req: Request, res: Response) => {
+//   res.clearCookie('jwtToken');
+//   return res.status(200).json({ message: 'Logged out successfully' });
+// });
+
 export default userRouter;
